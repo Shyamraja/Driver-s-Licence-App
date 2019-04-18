a61fd17201a971a7d41bcb08bc392d2acd832bcf
Shyamraja <t7bhsh00"students.oamk.fi>
2019-04-18 10:18:28 +0300

    Information activity showing next and previous lists of information

--- app/src/main/java/tapiopalonemi/fi/driversapp/Informationactivity.java
+++ app/src/main/java/tapiopalonemi/fi/driversapp/Informationactivity.java
@@ -2,16 +2,15 @@ package tapiopalonemi.fi.driversapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ViewFlipper;
 import java.util.ArrayList;
+import android.widget.Button;
 
-//import android.widget.myListView;
 
 public class Informationactivity extends AppCompatActivity {
+
 
     private ArrayList<String[]> listOfLists;
     private int index = -1;
@@ -23,22 +22,38 @@ public class Informationactivity extends AppCompatActivity {
         final String[] INFORMATIONS = new String[]{
                 "Adjust your mirrors correctly", "If the car don't start turn high beams on", "Use hand brake regularly",
                 "Don’t turn the wheels beforehand when making a left turn", "Maintain required distance always from other vehicles",
-                "Park with precision", "Be careful with signs on roads as well as traffic lights", "Always follow the speed limits",
-                "Give pedestrians the right-of-way in crosswalks", "Always check your seat belt"
-
+                "Park with precision", "follow the vehicle of neighbouring lane"
         };
 
+        ListView myListView= findViewById(R.id.information_list_view);
 
         final String[] INFORMATIONS2 = new String[]{
-                "Adjust your mirrors correctly", "If the car don't start turn high beams on", "Use hand brake regularly",
-                "Don’t turn the wheels beforehand when making a left turn", "Maintain required distance always from other vehicles",
-                "Park with precision", "Be careful with signs on roads as well as traffic lights", "Always follow the speed limits",
-                "Give pedestrians the right-of-way in crosswalks", "Always check your seat belt"
+                "Be careful with signs on roads as well as traffic lights", "Always follow the speed limits",
+                "Give pedestrians the right-of-way in crosswalks", "Always check your seat belt","Park with precision",
+                "Keep winter Survival kit in winter time","plan route in advanced for long trips"
 
         };
+
+        final String[] INFORMATIONS3 = new String[]{
+                "Don't take alcohol and drugs while drive", "Don't assume that other cars know what you are doing",
+                "Don't play loud music in your vehicle","Don't Talk on your cellphone and drive",
+                "Don't treat your vehicle like a toy","Don't leave your valueables in your car",
+                "Give priority to emergency vehicles"
+
+        };
+
+        final String[] INFORMATIONS4 = new String[]{
+                "If you face an accident make sure nobody in car is injured","Check the conditions of other side",
+                "Call Police immediately","Stay at the scene","Stay Calm and contact your insurance provider",
+                "Give your liscence and other documents to Insurance and Police","Don't be Abusive"
+
+        };
+
         listOfLists = new ArrayList<>();
         listOfLists.add(INFORMATIONS);
         listOfLists.add(INFORMATIONS2);
+        listOfLists.add(INFORMATIONS3);
+        listOfLists.add(INFORMATIONS4);
         nextInformation(null);
         previousInformation(null);
 
@@ -55,26 +70,41 @@ public class Informationactivity extends AppCompatActivity {
         myListView.setAdapter(kk);
     }
 
-    private void nextInformation(View view) {
+    public void nextInformation(View view) {
         index = index + 1;
-        if (index > listOfLists.size()) {
-            index = listOfLists.size() - 1;
-        }
-        Log.i("INFORMATIONS", "index: " + index);
-
+        setPreviousNextButtons(index);
         showInformation(listOfLists.get(index));
 
     }
 
-    private void previousInformation(View view) {
+    public void previousInformation(View view) {
         index = index - 1;
-        if (index < 0) {
-            index = 0;
-        }
-        Log.i("INFORMATIONS2", "index: " + index);
+        setPreviousNextButtons(index);
         showInformation(listOfLists.get(index));
 
     }
 
-}
+    private void setPreviousNextButtons(int idx) {
+        Button previousButton = findViewById(R.id.button2);
+        Button nextButton = findViewById(R.id.button3);
 
+        if (idx <= 0) {
+            index = 0;
+            previousButton.setEnabled(false);
+            nextButton.setEnabled(true);
+        } else {
+            previousButton.setEnabled(true);
+        }
+        if (index >= listOfLists.size() - 1) {
+            index = listOfLists.size() - 1;
+            nextButton.setEnabled(false);
+        } else {
+            if (index == listOfLists.size() - 1) {
+                nextButton.setEnabled(false);
+            } else {
+                nextButton.setEnabled(true);
+            }
+        }
+
+    }
+}
